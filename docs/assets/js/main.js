148a6e401828d417f9ae80d5676d94c086f021a8
Дмитрий <1primo1@mail.ru>
2024-02-28 22:56:20 +0300

    debug-6

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -184,6 +184,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   fixedHeader: () => (/* binding */ fixedHeader)
 /* harmony export */ });
 function fixedHeader() {
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
     let header = document.querySelector(".header__row_white");
     let row = document.querySelector(".header__row");
     let itemTop = header.getBoundingClientRect().top;
@@ -191,16 +192,20 @@ function fixedHeader() {
     window.addEventListener("scroll", getFixedHeader);
 
     function getFixedHeader() {
-        if (pageYOffset > itemTop && pageYOffset > 0) {
-            // header.style.cssText = `position: fixed; top:0`
+        if (mediaQuery.matches) {
             row.style.cssText = `display: none`
-        } else if (pageYOffset == 0) {
-            // header.style.cssText = `position: static;`
-            row.style.cssText = `display: flex`
-        }
-        else {
-            // header.style.cssText = `position: static;`
-            row.style.cssText = `display: flex`
+        } else {
+            if (pageYOffset > itemTop && pageYOffset > 0) {
+                // header.style.cssText = `position: fixed; top:0`
+                row.style.cssText = `display: none`
+            } else if (pageYOffset == 0) {
+                // header.style.cssText = `position: static;`
+                row.style.cssText = `display: flex`
+            }
+            else {
+                // header.style.cssText = `position: static;`
+                row.style.cssText = `display: flex`
+            }
         }
     }
 }
--- src/js/components/fixedHeader.js
+++ src/js/components/fixedHeader.js
@@ -1,4 +1,5 @@
 export function fixedHeader() {
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
     let header = document.querySelector(".header__row_white");
     let row = document.querySelector(".header__row");
     let itemTop = header.getBoundingClientRect().top;
@@ -6,16 +7,20 @@ export function fixedHeader() {
     window.addEventListener("scroll", getFixedHeader);
 
     function getFixedHeader() {
-        if (pageYOffset > itemTop && pageYOffset > 0) {
-            // header.style.cssText = `position: fixed; top:0`
+        if (mediaQuery.matches) {
             row.style.cssText = `display: none`
-        } else if (pageYOffset == 0) {
-            // header.style.cssText = `position: static;`
-            row.style.cssText = `display: flex`
-        }
-        else {
-            // header.style.cssText = `position: static;`
-            row.style.cssText = `display: flex`
+        } else {
+            if (pageYOffset > itemTop && pageYOffset > 0) {
+                // header.style.cssText = `position: fixed; top:0`
+                row.style.cssText = `display: none`
+            } else if (pageYOffset == 0) {
+                // header.style.cssText = `position: static;`
+                row.style.cssText = `display: flex`
+            }
+            else {
+                // header.style.cssText = `position: static;`
+                row.style.cssText = `display: flex`
+            }
         }
     }
 }
