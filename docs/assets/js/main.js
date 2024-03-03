4233881ef98593a13d42c3a3cf673d8dc49ac4b8
Дмитрий <1primo1@mail.ru>
2024-03-03 23:04:01 +0300

    reset

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -12803,7 +12803,7 @@ function mask() {
     var selector = document.querySelector(".phone");
     console.log(selector)
 
-    var im = new Inputmask("+7-999-999-99-99");
+    var im = new Inputmask("+7 (999) 999-99-99");
     im.mask(selector);
 }
 mask()
--- src/js/components/mask.js
+++ src/js/components/mask.js
@@ -2,7 +2,7 @@ export function mask() {
     var selector = document.querySelector(".phone");
     console.log(selector)
 
-    var im = new Inputmask("+7-999-999-99-99");
+    var im = new Inputmask("+7 (999) 999-99-99");
     im.mask(selector);
 }
 mask()
